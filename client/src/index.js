d16b2525597862ef2d604ff6238998dd53d4221e
CJ Lucido <c.jose.lucido@gmail.com>
2019-11-15 08:40:46 -0800

    outlined current architecture and made axiosWithAuth module

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,12 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
+
+//need an axiosWithAuth file
+//router is on App
+
